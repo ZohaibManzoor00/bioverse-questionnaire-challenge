0d152ceb2b09f92700d3519d03bbbc0afcc7480b
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2024-10-01 19:08:01 -0400

    fixed stale response to run everytime component mounts

--- app/(admin)/_components/submission-history.tsx
+++ app/(admin)/_components/submission-history.tsx
@@ -1,8 +1,20 @@
-import { loadSubmissionAggregates } from "../actions/loadSubmissionAggregates";
-import { SubmissionTable } from "./submission-table";
+"use client"
 
-export const SubmissionAggregates = async (): Promise<JSX.Element> => {
-  const submissionAggregates = await loadSubmissionAggregates();
+import { useEffect, useState } from "react";
+import { SubmissionAggregatesProps, SubmissionTable } from "./submission-table";
+import { loadSubmissionAggregates } from "../actions/loadSubmissionAggregates";
+
+export const SubmissionAggregates = (): JSX.Element => {
+  const [submissionAggregates, setSubmissionAggregates] = useState<SubmissionAggregatesProps[]>([]);
+  
+  const fetchAggregates = async () => {
+    const aggregates = await loadSubmissionAggregates();
+    setSubmissionAggregates(aggregates);
+  };
+
+  useEffect(() => {
+    fetchAggregates();
+  }, []); 
 
   return <SubmissionTable submissionAggregates={submissionAggregates} />;
 };
